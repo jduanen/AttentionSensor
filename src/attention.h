c0f7b9f5cf6d3c161eeb260a5995edf6ab6b2d8d
Duane Northcutt <duane.northcutt@gmail.com>
2024-07-10 17:22:00 -0700

    Created.

---
+++ src/attention.h
@@ -0,0 +1,14 @@
+/********************************************************************************
+ * 
+ * Attention Sensor
+ * 
+ *******************************************************************************/
+
+#ifndef ATTENTION_H
+#define ATTENTION_H
+
+#include <stdint.h>
+#include <Arduino.h>
+#include <USPS.h>
+
+#endif  // ATTENTION_H
