153e296dc98bcef5651a0732d47803d24d535563
Duane Northcutt <duane.northcutt@gmail.com>
2024-07-11 10:54:41 -0700

    Added ACTIVATE_PIN.

--- src/attention.cpp
+++ src/attention.cpp
@@ -9,6 +9,9 @@
 #include "attention.h"
 
 
+#define ACTIVATE_PIN    D3
+
+
 USPS *usps;
 
 
@@ -17,60 +20,24 @@ void setup() {
     while (!Serial) { ; };
     delay(500);
 
+    pinMode(ACTIVATE_PIN, OUTPUT);
+    digitalWrite(ACTIVATE_PIN, LOW);
+
     Wire.begin();
     usps = new USPS();
     Serial.println("BEGIN");  //// TMP TMP TMP
 };
 
+bool toggle = LOW;
+int cnt = 0;
+
 void loop() {
     if (usps->printFaces() > 0) {
         Serial.println("--------");
     }
-    delay(1000);
-};
-
-
-//VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
-
-/*
-  person_sensor_results_t results = {};
-  // Perform a read action on the I2C address of the sensor to get the
-  // current face information detected.
-  if (!person_sensor_read(&results)) {
-    Serial.println("No person sensor results found on the i2c bus");
-    delay(SAMPLE_DELAY_MS);
-    return;
-  }
-  
-  if (results.num_faces < 1) {
-    delay(SAMPLE_DELAY_MS);
-    return;
-  }
-
-  Serial.println("********");
-  Serial.print(results.num_faces);
-  Serial.println(" faces found");
-  for (int i = 0; i < results.num_faces; ++i) {
-    const person_sensor_face_t* face = &results.faces[i];
-    Serial.print("Face #");
-    Serial.print(i);
-    Serial.print(": ");
-    Serial.print(face->box_confidence);
-    Serial.print(" confidence, (");
-    Serial.print(face->box_left);
-    Serial.print(", ");
-    Serial.print(face->box_top);
-    Serial.print("), (");
-    Serial.print(face->box_right);
-    Serial.print(", ");
-    Serial.print(face->box_bottom);
-    Serial.print("), ");
-    if (face->is_facing) {
-      Serial.println("facing");
-    } else {
-      Serial.println("not facing");
+    digitalWrite(ACTIVATE_PIN, toggle);
+    if ((cnt % 100) == 0) {
+        toggle = (toggle == HIGH) ? LOW : HIGH;
     }
-  }
-  delay(SAMPLE_DELAY_MS);
-}
-*/
+    cnt++;
+};
