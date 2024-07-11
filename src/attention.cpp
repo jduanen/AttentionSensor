88251fe5f8374c8525bf6047f2d95900adbdbaed
Duane Northcutt <duane.northcutt@gmail.com>
2024-07-11 16:09:18 -0700

    Made work for more than one face.

--- src/attention.cpp
+++ src/attention.cpp
@@ -26,22 +26,32 @@ void setup() {
 
     Wire.begin();
     usps = new USPS();
-    Serial.println("BEGIN");  //// TMP TMP TMP
+    //Serial.println("BEGIN");  //// TMP TMP TMP
 };
 
-//// FIXME make this work for multiple faces
 void loop() {
-    USPSface_t faces;
+    USPSface_t faces[USPS_MAX_FACES];
     int8_t numFaces;
 
-    numFaces = usps->getFaces(&faces, 1);
-    if (numFaces && (faces.boxConfidence >= MIN_CONFIDENCE)) {
+    int8_t n = usps->getFaces(faces, USPS_MAX_FACES);
+    numFaces = n;
+    for (int i = 0; (i < n); i++) {
+//        usps->printFace(faces[i]);
+        if (!faces[i].isFacing || (faces[i].boxConfidence < MIN_CONFIDENCE)) {
+            numFaces--;
+//            Serial.print("F: ");Serial.print(faces[i].isFacing); Serial.print(", ");
+//            Serial.println(faces[i].boxConfidence);
+        }
+    }
+//    Serial.print("N: ");Serial.print(n); Serial.print(", "); Serial.println(numFaces);
+    if (numFaces) {
         detects++;
         if (!active) {
             if (detects >= DETECT_COUNT) {
                 active = true;
                 activeTime = millis();
                 digitalWrite(ACTIVATE_PIN, HIGH);
+                //Serial.println("ACTIVE");
             }
         }
     } else {
@@ -50,6 +60,7 @@ void loop() {
             if ((millis() - activeTime) > MIN_ACTIVE_MS) {
                 active = false;
                 digitalWrite(ACTIVATE_PIN, LOW);
+                //Serial.println("INACTIVE");
             }
         }
     }
