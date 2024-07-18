ef48d175193edb54837157a855c87c47f4049771
Duane Northcutt <duane.northcutt@gmail.com>
2024-07-18 14:23:44 -0700

    Changed active pin and detect count.

--- src/attention.h
+++ src/attention.h
@@ -2,6 +2,12 @@
  * 
  * Attention Sensor
  * 
+ * Xiao RP2040 has a power LED (Red), a user-programmable RGB LED, and a NEOPixel.
+ *   LED_BUILTIN = 17
+ *   PIN_LED_R = 17
+ *   PIN_LED_G = 16
+ *   PIN_LED_B = 25
+ *
  *******************************************************************************/
 
 #ifndef ATTENTION_H
@@ -12,13 +18,13 @@
 #include <USPS.h>
 
 
-#define ACTIVATE_PIN    D3
+#define ACTIVATE_PIN    D2
 
 // minimum confidence level required to qualify as a match [0-100]
 #define MIN_CONFIDENCE  85
 
 // number of consecutive detections required to trigger
-#define DETECT_COUNT    3
+#define DETECT_COUNT    2
 
 // minimum active time (msec)
 #define MIN_ACTIVE_MS   10000
