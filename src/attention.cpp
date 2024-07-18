ba9bc1b81900e21448e0906be15e63cd691a77f3
Duane Northcutt <duane.northcutt@gmail.com>
2024-07-18 14:24:33 -0700

    Fixed millis variable. Added LED output. Removed Serial use.

--- src/attention.cpp
+++ src/attention.cpp
@@ -13,20 +13,29 @@ USPS *usps;
 
 bool active = false;
 uint8_t detects = 0;
-uint16_t activeTime;
+unsigned long activeTime;
+
+void (* resetFunc)(void) = 0;
 
 
 void setup() {
-    Serial.begin(115200);
-    while (!Serial) { ; };
+//    Serial.begin(115200);
+//    while (!Serial) { ; };
     delay(500);
 
     pinMode(ACTIVATE_PIN, OUTPUT);
     digitalWrite(ACTIVATE_PIN, LOW);
 
+    pinMode(PIN_LED_R, OUTPUT);
+    digitalWrite(PIN_LED_R, HIGH);
+    pinMode(PIN_LED_G, OUTPUT);
+    digitalWrite(PIN_LED_G, HIGH);
+    pinMode(PIN_LED_B, OUTPUT);
+    digitalWrite(PIN_LED_B, HIGH);
+
     Wire.begin();
     usps = new USPS();
-    //Serial.println("BEGIN");  //// TMP TMP TMP
+//    Serial.println("BEGIN");  //// TMP TMP TMP
 };
 
 void loop() {
@@ -35,6 +44,7 @@ void loop() {
 
     int8_t n = usps->getFaces(faces, USPS_MAX_FACES);
     numFaces = n;
+/*
     for (int i = 0; (i < n); i++) {
 //        usps->printFace(faces[i]);
         if (!faces[i].isFacing || (faces[i].boxConfidence < MIN_CONFIDENCE)) {
@@ -44,6 +54,15 @@ void loop() {
         }
     }
 //    Serial.print("N: ");Serial.print(n); Serial.print(", "); Serial.println(numFaces);
+*/
+    if (numFaces < 0) {
+//        Serial.println("ERROR: failed to read, resetting...");
+        digitalWrite(PIN_LED_R, LOW);
+        digitalWrite(PIN_LED_G, HIGH);
+        digitalWrite(PIN_LED_B, HIGH);
+        resetFunc();
+        return;
+    }
     if (numFaces) {
         detects++;
         if (!active) {
@@ -51,7 +70,10 @@ void loop() {
                 active = true;
                 activeTime = millis();
                 digitalWrite(ACTIVATE_PIN, HIGH);
-                //Serial.println("ACTIVE");
+//                Serial.println("ACTIVE");
+                digitalWrite(PIN_LED_R, LOW);
+                digitalWrite(PIN_LED_G, LOW);
+                digitalWrite(PIN_LED_B, LOW);
             }
         }
     } else {
@@ -60,10 +82,20 @@ void loop() {
             if ((millis() - activeTime) > MIN_ACTIVE_MS) {
                 active = false;
                 digitalWrite(ACTIVATE_PIN, LOW);
-                //Serial.println("INACTIVE");
+//                Serial.println("INACTIVE");
+                digitalWrite(PIN_LED_R, HIGH);
+                digitalWrite(PIN_LED_G, HIGH);
+                digitalWrite(PIN_LED_B, HIGH);
             }
         }
     }
+    /*
+    Serial.print(numFaces); Serial.print(" ");
+    Serial.print(active); Serial.print(" ");
+    Serial.print(detects); Serial.print(" ");
+    Serial.print(activeTime); Serial.print(" ");
+    Serial.println(millis());
+    */
 
     delay(LOOP_DELAY);
 };
